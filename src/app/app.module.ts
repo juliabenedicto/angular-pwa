7e40b3c5950ce86ab7c911e0040fe889465c3753
juliabenedicto <74271084+juliabenedicto@users.noreply.github.com>
2022-01-08 15:41:23 +0100

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ImagesComponent } from './components/images/images.component';
+import { ImageComponent } from './components/image/image.component';
+
+import { MatCardModule } from '@angular/material/card';
+
+
+import {HttpClientModule} from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ImagesComponent,
+    ImageComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatCardModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
